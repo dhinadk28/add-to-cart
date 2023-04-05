114f728ea5d2d25f6197cc19156968d83d800ad8
dhinadk28 <dhinadk28@gmail.com>
2023-04-05 15:26:37 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
-import './App.css'; // import custom CSS file for styling
+import './App.css';
 
 const products = [
   {
@@ -24,6 +24,27 @@ const products = [
     rating: 3,
     image: 'https://via.placeholder.com/150',
   },
+  {
+    id: 4,
+    name: 'Product 4',
+    price: 40,
+    rating: 4,
+    image: 'https://via.placeholder.com/150',
+  },
+  {
+    id: 5,
+    name: 'Product 5',
+    price: 50,
+    rating: 5,
+    image: 'https://via.placeholder.com/150',
+  },
+  {
+    id: 6,
+    name: 'Product 6',
+    price: 60,
+    rating: 3,
+    image: 'https://via.placeholder.com/150',
+  },
 ];
 
 const App = () => {
@@ -41,68 +62,55 @@ const App = () => {
 
   return (
     <div className="app">
-    <Container className="my-4">
-      <h1 className="text-center mb-4">Shop Homepage</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product.id} className="mb-4">
-            <Card>
-              <Row noGutters>
-                <Col md={4}>
-                  <Card.Img src={product.image} />
-                </Col>
-                <Col md={8}>
-                  <Card.Body>
-                    <Card.Title>{product.name}</Card.Title>
-                    <Card.Text>
-                      {product.description}
-                    </Card.Text>
-                    <div className="d-flex align-items-center justify-content-between mb-3">
-                      <span>Price: {product.price}</span>
-                      <div>
-                        <span className="mr-2">Rating: {product.rating}</span>
-                        <Button
-                          variant="primary"
-                          disabled={cart.find((item) => item.id === product.id)}
-                          onClick={() => handleAddToCart(product)}
-                        >
-                          {cart.find((item) => item.id === product.id)
-                            ? 'Added to cart'
-                            : 'Add to cart'}
-                        </Button>
-                      </div>
-                    </div>
-                  </Card.Body>
-                </Col>
-              </Row>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-      {cart.length > 0 && (
-        <div className="mt-4">
-          <h2>Shopping Cart</h2>
-          <ul className="list-group">
-            {cart.map((item) => (
-              <li
-                key={item.id}
-                className="list-group-item d-flex justify-content-between align-items-center"
-              >
-                {item.name} - {item.price}
-                <button
-                  className="btn btn-sm btn-danger"
-                  onClick={() => handleRemoveFromCart(item.id)}
+      <Container fluid className="my-4">
+        <h1 className="text-center mb-4">Shop Homepage</h1>
+        <Row>
+          {products.map((product) => (
+            <Col key={product.id} md={4} className="mb-4">
+              <Card>
+                <Card.Img variant="top" src={product.image} />
+                <Card.Body>
+                  <Card.Title>{product.name}</Card.Title>
+                  <Card.Text>Price: {product.price}</Card.Text>
+                  <Card.Text>Rating: {product.rating}</Card.Text>
+                  <Button
+                    variant="primary"
+                    disabled={cart.find((item) => item.id === product.id)}
+                    onClick={() => handleAddToCart(product)}
+                  >
+                    {cart.find((item) => item.id === product.id)
+                      ? 'Added to cart'
+                      : 'Add to cart'}
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+        {cart.length > 0 && (
+          <div className="mt-4">
+            <h2>Shopping Cart</h2>
+            <ul className="list-group">
+              {cart.map((item) => (
+                <li
+                  key={item.id}
+                  className="list-group-item d-flex justify-content-between align-items-center"
                 >
-                  Remove
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </Container>
-  </div>
-  )  
+                  {item.name} - {item.price}
+                  <button
+                    className="btn btn-sm btn-danger"
+                    onClick={() => handleRemoveFromCart(item.id)}
+                  >
+                    Remove
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </Container>
+    </div>
+  );
 };
 
 export default App;
